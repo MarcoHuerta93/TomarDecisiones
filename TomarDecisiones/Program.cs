d3293383c10faf7982d8e155cffae0da7fe4de4a
Marco Huerta <marco.huerta.sc@pm.me>
2021-07-19 13:14:16 -0500

    if anidado

--- TomarDecisiones/Program.cs
+++ TomarDecisiones/Program.cs
@@ -6,39 +6,65 @@ namespace TomarDecisiones
     {
         public static void Main(string[] args)
         {
-            string temperatura = Console.ReadLine();
+            //if anidados
 
-            int numTemperatura;
+            bool esAdmin = false;
+            bool estaRegistrado = true;
+            string nombreDeUsuario = "";
 
-            int numero;
-            if (int.TryParse(temperatura, out numero)) {
+            Console.WriteLine("Por favor ingrese su nombre de usuario");
 
-                numTemperatura = numero;
-            }
-            else
-            {
-                numTemperatura = 0;
-                Console.WriteLine("El valor ingresado no es válido, se estableció que la temperatura es 0" +
-                    "");
-            }
+            nombreDeUsuario = Console.ReadLine();
 
-            if (numTemperatura < 20)
+            if (estaRegistrado && nombreDeUsuario != "" && nombreDeUsuario.Equals("Admin"))
             {
-                Console.WriteLine("Abrígate");
+                Console.WriteLine("Hola Usuario registrado");
+                    Console.WriteLine("Hola " + nombreDeUsuario);
+                        Console.WriteLine("Hola Admin");
+                    
+                }
+            if(esAdmin || estaRegistrado)
+            {
+                Console.WriteLine("Estás loguado");
+            }
+            
 
-            }
-            else if (numTemperatura == 20)
-            {
-                Console.WriteLine("Vístete cómodo!!");
-            }
-           else if (numTemperatura > 30)
-            {
-                Console.WriteLine("Hacen 30 grados, que calor!!");
-            }
-            else 
-            {
-                Console.WriteLine("Usa ropa bien liviana!!");
-            }
+            //TRYPARSE
+
+            /* string temperatura = Console.ReadLine();
+
+             int numTemperatura;
+
+             int numero;
+             if (int.TryParse(temperatura, out numero)) {
+
+                 numTemperatura = numero;
+             }
+             else
+             {
+                 numTemperatura = 0;
+                 Console.WriteLine("El valor ingresado no es válido, se estableció que la temperatura es 0" +
+                     "");
+             }
+
+             if (numTemperatura < 20)
+             {
+                 Console.WriteLine("Abrígate");
+
+             }
+             else if (numTemperatura == 20)
+             {
+                 Console.WriteLine("Vístete cómodo!!");
+             }
+            else if (numTemperatura > 30)
+             {
+                 Console.WriteLine("Hacen 30 grados, que calor!!");
+             }
+             else 
+             {
+                 Console.WriteLine("Usa ropa bien liviana!!");
+             }
+             Console.Read();*/
             Console.Read();
         }
     }
