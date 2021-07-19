0ce3e267c89f8c64bdbcb4d92e3cf27d776de7fd
Marco Huerta <marco.huerta.sc@pm.me>
2021-07-19 12:57:04 -0500

    TRY PARSE

--- TomarDecisiones/Program.cs
+++ TomarDecisiones/Program.cs
@@ -8,18 +8,34 @@ namespace TomarDecisiones
         {
             string temperatura = Console.ReadLine();
 
-            int numTemperatura = int.Parse(temperatura);
+            int numTemperatura;
+
+            int numero;
+            if (int.TryParse(temperatura, out numero)) {
+
+                numTemperatura = numero;
+            }
+            else
+            {
+                numTemperatura = 0;
+                Console.WriteLine("El valor ingresado no es válido, se estableció que la temperatura es 0" +
+                    "");
+            }
 
             if (numTemperatura < 20)
             {
                 Console.WriteLine("Abrígate");
 
             }
-            if (numTemperatura == 20)
+            else if (numTemperatura == 20)
             {
-                Console.WriteLine("Vístete cómodo");
+                Console.WriteLine("Vístete cómodo!!");
             }
-            if (numTemperatura > 20)
+           else if (numTemperatura > 30)
+            {
+                Console.WriteLine("Hacen 30 grados, que calor!!");
+            }
+            else 
             {
                 Console.WriteLine("Usa ropa bien liviana!!");
             }
